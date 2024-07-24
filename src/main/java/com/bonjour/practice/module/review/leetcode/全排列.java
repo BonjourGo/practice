8c20d7567729bf48062cb6784dc313535fad211f
tc <1510298763@qq.com>
2024-07-24 22:24:42 +0800

    init

---
+++ src/main/java/com/bonjour/practice/module/review/leetcode/全排列.java
@@ -0,0 +1,29 @@
+package com.bonjour.practice.module.review.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class 全排列 {
+
+    // 给定一个不含重复数字的数组 nums ，返回其 所有可能的全排列 。你可以 按任意顺序 返回答案。
+    //示例 1：
+    //输入：nums = [1,2,3]
+    //输出：[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+    //示例 2：
+    //输入：nums = [0,1]
+    //输出：[[0,1],[1,0]]
+    //示例 3：
+    //输入：nums = [1]
+    //输出：[[1]]
+    // 1234
+    // 1234 1324 1423 1342 1432 1342
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        int[] ints = new int[]{1, 2, 3};
+    }
+}
